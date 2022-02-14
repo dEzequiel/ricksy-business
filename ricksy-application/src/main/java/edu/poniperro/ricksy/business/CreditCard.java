2dbf63509798242fb5a13229e54d72081664445f
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-15 00:27:19 +0100

    feat(CreditCard): add attributes

---
+++ ricksy-application/src/main/java/edu/poniperro/ricksy/business/CreditCard.java
@@ -0,0 +1,10 @@
+package edu.poniperro.ricksy.business;
+
+public class CreditCard {
+    private final String owner;
+    private final String number;
+    private double credit = 3000;
+    private final String SYMBOL;
+
+    
+}

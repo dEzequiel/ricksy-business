ef07391e87c289c42f253046c22c7a228a70fb56
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-15 00:29:04 +0100

    feat(CreditCard): add constructor

--- ricksy-application/src/main/java/edu/poniperro/ricksy/business/CreditCard.java
+++ ricksy-application/src/main/java/edu/poniperro/ricksy/business/CreditCard.java
@@ -4,7 +4,10 @@ public class CreditCard {
     private final String owner;
     private final String number;
     private double credit = 3000;
-    private final String SYMBOL;
-
+    // private final String SYMBOL;
     
+    CreditCard(String owner, String number) {
+        this.owner = owner;
+        this.number = number;
+    }
 }

06bab82b87c0d1d377d3fb1c191347d954ca7159
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-15 19:48:04 +0100

    feat(cardNumbers): add method to return a collection made by flota values

--- ricksy-application/src/main/java/edu/poniperro/ricksy/business/UfosPark.java
+++ ricksy-application/src/main/java/edu/poniperro/ricksy/business/UfosPark.java
@@ -1,7 +1,10 @@
 package edu.poniperro.ricksy.business;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class UfosPark implements GuestDispatcher{
     private double fee = 500;
@@ -31,7 +34,7 @@ public class UfosPark implements GuestDispatcher{
             for (Map.Entry<String, String> entry : this.flota.entrySet()) {
                 if (entry.getValue() == creditCardNumber) {
                     ufoID = entry.getKey();
-                    break;
+                    break; // finish loop and go to return
                 }
             }
         }
@@ -44,30 +47,34 @@ public class UfosPark implements GuestDispatcher{
         } else {return false;}
     }
 
-        // pago y reserva
-        @Override
-        public void dispatch(CreditCard creditCard) {
-    
-            //create map entyr, key/value, to insert into flota
-            Map.Entry<String, String> ufo = null;
-    
-            // if creditcard already have a ufo assigned, abort mission
-            if (!this.flota.containsValue(creditCard.number())) {
-                for(Map.Entry<String, String> entry : this.flota.entrySet()) {
-                    if(entry.getValue() == null) {
-                        ufo = entry;
-                        break;
-                    }
+   Collection<String> cardNumbers() {
+        return this.flota.values();
+    }
+
+    // pago y reserva
+    @Override
+    public void dispatch(CreditCard creditCard) {
+
+        //create map entyr, key/value, to insert into flota
+        Map.Entry<String, String> ufo = null;
+
+        // if creditcard already have a ufo assigned, abort mission
+        if (!this.flota.containsValue(creditCard.number())) {
+            for(Map.Entry<String, String> entry : this.flota.entrySet()) {
+                if(entry.getValue() == null) {
+                    ufo = entry;
+                    break;
                 }
             }
-    
-            if (ufo != null && creditCard.pay(this.fee)) {
-                this.flota.put(ufo.getKey(), creditCard.number());
-            }
-    
-    
         }
-    
+
+        if (ufo != null && creditCard.pay(this.fee)) {
+            this.flota.put(ufo.getKey(), creditCard.number());
+        }
+
+
+    }
+
     @Override
     public String toString()  {
         StringBuilder output = new StringBuilder();

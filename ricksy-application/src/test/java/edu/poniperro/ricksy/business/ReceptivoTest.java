b4e99d81a4e69dff92ccf1906c9c6be7d7d70a70
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-15 21:28:34 +0100

    test(dispatchObservers): test to check if crystalexpender stock decrease and ufo id is assigned to a number, also the creditcard credit decreaso equally to the services waste

---
+++ ricksy-application/src/test/java/edu/poniperro/ricksy/business/ReceptivoTest.java
@@ -0,0 +1,43 @@
+package edu.poniperro.ricksy.business;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class ReceptivoTest {
+
+    @Test
+    public void addingOberservers() {
+        UfosPark ufosPark = new UfosPark();
+        CrystalExpender expender = new CrystalExpender(3, 50);
+        CreditCard creditCard = new CreditCard("Abradolph Lincler", "1233545");
+        Receptivo receptivo = new Receptivo();  
+        
+        receptivo.registra(ufosPark);
+        receptivo.registra(expender);
+
+        assertEquals(2, receptivo.getObservers());
+    }
+
+    @Test
+    public void dispatchObservers() {
+
+        UfosPark ufosPark = new UfosPark();
+        ufosPark.add("unox");
+
+        CrystalExpender expender = new CrystalExpender(3, 50);
+        CreditCard creditCard = new CreditCard("Abradolph Lincler", "1233545");
+        Receptivo receptivo = new Receptivo();  
+        
+        receptivo.registra(ufosPark);
+        receptivo.registra(expender);
+
+        receptivo.dispatch(creditCard);
+
+        assertEquals(2, expender.stock());
+        assertTrue("Ufo assigned", ufosPark.containsCard(creditCard.number()));
+        assertEquals(2450, creditCard.credit(), 0);
+    }
+    
+}

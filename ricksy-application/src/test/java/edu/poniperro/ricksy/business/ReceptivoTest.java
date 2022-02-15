334514d6ba11d7ec76f2a5ddf3de092c31ac2537
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-15 21:32:10 +0100

    no-relevant: delete unused objects and imports

--- ricksy-application/src/test/java/edu/poniperro/ricksy/business/ReceptivoTest.java
+++ ricksy-application/src/test/java/edu/poniperro/ricksy/business/ReceptivoTest.java
@@ -1,8 +1,5 @@
 package edu.poniperro.ricksy.business;
-
 import org.junit.Test;
-
-
 import static org.junit.Assert.*;
 
 public class ReceptivoTest {
@@ -11,7 +8,6 @@ public class ReceptivoTest {
     public void addingOberservers() {
         UfosPark ufosPark = new UfosPark();
         CrystalExpender expender = new CrystalExpender(3, 50);
-        CreditCard creditCard = new CreditCard("Abradolph Lincler", "1233545");
         Receptivo receptivo = new Receptivo();  
         
         receptivo.registra(ufosPark);

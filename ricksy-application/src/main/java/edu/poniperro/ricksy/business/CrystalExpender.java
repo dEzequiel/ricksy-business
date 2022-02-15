31a350ee6097e73f89735eca9a1b8144d9443cd1
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-15 18:36:51 +0100

    feat(CrystalExpender): add class constructor

--- ricksy-application/src/main/java/edu/poniperro/ricksy/business/CrystalExpender.java
+++ ricksy-application/src/main/java/edu/poniperro/ricksy/business/CrystalExpender.java
@@ -3,5 +3,9 @@ package edu.poniperro.ricksy.business;
 public class CrystalExpender implements GuestDispatcher{
     private int stock;
     private double itemCost;
-    
+
+    public CrystalExpender(int stock, double itemCost) {
+        this.stock = stock;
+        this.itemCost = itemCost;
+    }
 }

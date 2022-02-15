3b049fb7d78deca88507cc898286f4f12631938c
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-15 19:24:58 +0100

    feat(Receptivo): add method to observe other behaviours

---
+++ ricksy-application/src/main/java/edu/poniperro/ricksy/business/Receptivo.java
@@ -0,0 +1,21 @@
+package edu.poniperro.ricksy.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Receptivo {
+
+    List<GuestDispatcher> observers = new ArrayList<GuestDispatcher>();
+
+    void registra(GuestDispatcher observer) {
+        observers.add(observer);
+    }
+
+
+    void dispatch(CreditCard number) {
+        for(GuestDispatcher observer : observers) {
+            observer.dispatch(number);
+        }
+    }
+
+}

abc703d3b642a910138eb5e7a124fe4fa01bbd98
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-15 20:33:26 +0100

    test(checkIfStockDecreaseAndCreditDecrease): add method to check if crystalexpender stock decrease and the credit card credit too

---
+++ ricksy-application/src/test/java/edu/poniperro/ricksy/business/CrystalExpenderTest.java
@@ -0,0 +1,24 @@
+package edu.poniperro.ricksy.business;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class CrystalExpenderTest {
+    
+    @Test
+    public void checkIfStockDecreaseAndCreditDecrease() {
+        CrystalExpender expender = new CrystalExpender(3, 50);
+        CreditCard creditCard = new CreditCard("Abradolph Lincler", "1233545");
+        
+        expender.dispatch(creditCard);
+
+        assertEquals(2, expender.stock());
+        assertEquals(2950, creditCard.credit(), 0);
+
+        expender.dispatch(creditCard);
+        expender.dispatch(creditCard);
+
+        assertEquals(0, expender.stock());
+    }
+}

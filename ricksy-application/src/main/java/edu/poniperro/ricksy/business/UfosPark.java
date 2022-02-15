7754994fe28f18da95b240fb7837d2f62d89f31a
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-15 13:53:02 +0100

    feat(dispatch): add method to pay ufos

--- ricksy-application/src/main/java/edu/poniperro/ricksy/business/UfosPark.java
+++ ricksy-application/src/main/java/edu/poniperro/ricksy/business/UfosPark.java
@@ -6,4 +6,11 @@ import java.util.Map;
 public class UfosPark implements GuestDispatcher{
     private double fee;
     private final Map<String, String> flota = new HashMap<>();
+
+    UfosPark(){};
+
+    @Override
+    public void dispatch(CreditCard creditCard) {
+        creditCard.pay(this.fee);
+    }
 }
